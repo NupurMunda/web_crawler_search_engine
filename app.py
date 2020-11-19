5be388e486858cc695bfc5716ef3024fd3c2b38d
NupurMunda <nupurmunda66@gmail.com>
2020-11-19 17:54:03 +0530

    heroku regarding changes

--- app.py
+++ app.py
@@ -73,6 +73,7 @@ def page_not_found(e):
 
 
 if __name__ == '__main__':
+    app.run()
     
 		# @app.route('/js/<fname>')
 		# def send_js(fname):
@@ -91,6 +92,7 @@ if __name__ == '__main__':
 			# This is used when running locally only. When deploying to Google App
 			# Engine, a webserver process such as Gunicorn will serve the app. This
 			# can be configured by adding an `entrypoint` to app.yaml.
-		app.run(host='127.0.0.1', port=8080, debug=True)
+		#app.run(host='127.0.0.1', port=8080, debug=True)
+  
 
 # [END app]
